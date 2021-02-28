7734432102231e147772b45c0fcc6490511e6c8f
Emile EKANE <ekaneekane3@gmail.com>
2021-02-28 14:56:05 +0100

    Update mod1.py
    scrap scrap

--- mod1.py
+++ mod1.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Feb 28 14:32:46 2021
-
-@author: EKANE EMILE
+@author: EKANE3
 Module 1 : Scrapping
 """
 import pandas as pd
 
-#Importer le csv dans un DataFrame pandas.
+#Import the csv into a DataFrame
 #url:https://cadastre.data.gouv.fr/data/etalab-dvf/latest/csv/2020/full.csv.gz
+
 data = pd.read_csv("D:/full.csv")
 df = pd.DataFrame(data)
-#Ajouter une colonne contenant votre nom.
-df['myName'] = 'EKANE'
-#Ajouter une colonne portant le nom « adresse_string 
-#NUMERO_RUE NOM_RUE NOM_VILLE CODE_POSTAL PAYS
+
+#Add a column myName using your name.
+df['myName'] = 'EKANE3'
+
+#Add a column adresse_string based on existing column
+#adresse_string must contain : NUMERO_RUE NOM_RUE NOM_VILLE CODE_POSTAL PAYS
+
 df['adresse_string'] = df['adresse_numero'].map(str) + df['adresse_nom_voie'].map(str) +" "+df['nom_commune'].map(str) +" "+df['code_postal'].map(str) +" FRANCE"
-#Supprimer les lignes ayant les colonnes « longitude » et « latitude » à vide
+#Delete lines having an empty "longitude" and "latitude" column 
 df.dropna(subset = ["longitude","latitude"], inplace=True)
+#Print the first ten lines of the dataframe
 print(df.head(10))
+#Print the dataframe
+print(df)
 
 
 
